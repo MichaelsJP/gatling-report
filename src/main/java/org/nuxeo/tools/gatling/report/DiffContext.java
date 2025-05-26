aabd745a0634b8ceb490c2b2f519db566994dbda
Julian Psotta <julianpsotta@gmail.com>
2025-05-26 08:33:34 +0200

    feat: Repair percent values

--- src/main/java/org/nuxeo/tools/gatling/report/DiffContext.java
+++ src/main/java/org/nuxeo/tools/gatling/report/DiffContext.java
@@ -30,6 +30,12 @@ public class DiffContext {
 
     protected List<String> scripts;
 
+    // Constants to avoid duplication
+    private static final String WIN = "win";
+    private static final String LOOSE = "loose";
+    private static final String FORMAT_PERCENT = "%+.2f";
+    private static final Locale LOCALE = Locale.ENGLISH;
+
     public DiffContext(List<SimulationContext> stats) {
         ref = stats.get(0);
         challenger = stats.get(1);
@@ -57,25 +63,68 @@ public class DiffContext {
     }
 
     public String avgPercent() {
-        return String.format(Locale.ENGLISH, "%+.2f", (challenger.simStat.avg * 100.0 / ref.simStat.avg) - 100.0);
+        return String.format(LOCALE, FORMAT_PERCENT, (challenger.simStat.avg * 100.0 / ref.simStat.avg) - 100.0);
     }
 
     public String avgClass() {
         if (challenger.simStat.avg < ref.simStat.avg) {
-            return "win";
+            return WIN;
         }
-        return "loose";
+        return LOOSE;
     }
 
     public String rpsPercent() {
-        return String.format(Locale.ENGLISH, "%+.2f", (challenger.simStat.rps * 100.0 / ref.simStat.rps) - 100.0);
+        return String.format(LOCALE, FORMAT_PERCENT, (challenger.simStat.rps * 100.0 / ref.simStat.rps) - 100.0);
     }
 
     public String rpsClass() {
         if (challenger.simStat.rps > ref.simStat.rps) {
-            return "win";
+            return WIN;
         }
-        return "loose";
+        return LOOSE;
+    }
+
+    public String durationDiff() {
+        double diff = challenger.simStat.duration - ref.simStat.duration;
+        return String.format(LOCALE, FORMAT_PERCENT, diff);
+    }
+
+    public String durationClass() {
+        if (challenger.simStat.duration < ref.simStat.duration) {
+            return WIN;
+        }
+        return LOOSE;
+    }
+
+    public String requestCountDiff() {
+        double percent = ((double) challenger.simStat.count / ref.simStat.count * 100.0) - 100.0;
+        return String.format(LOCALE, FORMAT_PERCENT, percent);
+    }
+
+    public String requestCountClass() {
+        return challenger.simStat.count >= ref.simStat.count ? WIN : LOOSE;
+    }
+
+    public String errorCountDiff() {
+        return String.format(LOCALE, "%+d", challenger.simStat.errorCount - ref.simStat.errorCount);
+    }
+
+    public String errorCountClass() {
+        if (challenger.simStat.errorCount < ref.simStat.errorCount) {
+            return WIN;
+        }
+        return LOOSE;
+    }
+
+    public String maxUsersDiff() {
+        double percent = ((double) challenger.simStat.maxUsers / ref.simStat.maxUsers * 100.0) - 100.0;
+        return String.format(LOCALE, FORMAT_PERCENT, percent);
+    }
+
+    public String maxUsersClass() {
+        // More concurrent users could be better or worse depending on perspective
+        // Here we assume more users is better (higher load testing capability)
+        return challenger.simStat.maxUsers >= ref.simStat.maxUsers ? WIN : LOOSE;
     }
 
 }
--- src/main/java/org/nuxeo/tools/gatling/report/DiffRequestStat.java
+++ src/main/java/org/nuxeo/tools/gatling/report/DiffRequestStat.java
@@ -16,6 +16,8 @@
  */
 package org.nuxeo.tools.gatling.report;
 
+import java.util.Locale;
+
 public class DiffRequestStat {
     protected final RequestStat refR;
 
@@ -24,5 +26,118 @@ public class DiffRequestStat {
     public DiffRequestStat(RequestStat refStat, RequestStat challengerStat) {
         refR = refStat;
         challengerR = challengerStat;
+    }
+
+    // Constants to avoid duplication
+    private static final String ZERO_PERCENT = "+0.00";
+    private static final String FORMAT_PERCENT = "%+.2f";
+    private static final String WIN = "win";
+    private static final String LOOSE = "loose";
+    private static final Locale LOCALE = Locale.ENGLISH;
+
+    public String minPercent() {
+        if (refR.min == 0) {
+            return ZERO_PERCENT;
+        }
+        return String.format(LOCALE, FORMAT_PERCENT, (challengerR.min * 100.0 / refR.min) - 100.0);
+    }
+
+    public String minClass() {
+        if (challengerR.min < refR.min) {
+            return WIN;
+        }
+        return LOOSE;
+    }
+
+    public String avgRPercent() {
+        if (refR.avg == 0) {
+            return ZERO_PERCENT;
+        }
+        return String.format(LOCALE, FORMAT_PERCENT, (challengerR.avg * 100.0 / refR.avg) - 100.0);
+    }
+
+    public String avgRClass() {
+        if (challengerR.avg < refR.avg) {
+            return WIN;
+        }
+        return LOOSE;
+    }
+
+    public String maxPercent() {
+        if (refR.max == 0) {
+            return ZERO_PERCENT;
+        }
+        return String.format(LOCALE, FORMAT_PERCENT, (challengerR.max * 100.0 / refR.max) - 100.0);
+    }
+
+    public String maxClass() {
+        if (challengerR.max < refR.max) {
+            return WIN;
+        }
+        return LOOSE;
+    }
+
+    public String p50Percent() {
+        if (refR.p50 == 0) {
+            return ZERO_PERCENT;
+        }
+        return String.format(LOCALE, FORMAT_PERCENT, (challengerR.p50 * 100.0 / refR.p50) - 100.0);
+    }
+
+    public String p50Class() {
+        if (challengerR.p50 < refR.p50) {
+            return WIN;
+        }
+        return LOOSE;
+    }
+
+    public String p95Percent() {
+        if (refR.p95 == 0) {
+            return ZERO_PERCENT;
+        }
+        return String.format(LOCALE, FORMAT_PERCENT, (challengerR.p95 * 100.0 / refR.p95) - 100.0);
+    }
+
+    public String p95Class() {
+        if (challengerR.p95 < refR.p95) {
+            return WIN;
+        }
+        return LOOSE;
+    }
+
+    public String p99Percent() {
+        if (refR.p99 == 0) {
+            return ZERO_PERCENT;
+        }
+        return String.format(LOCALE, FORMAT_PERCENT, (challengerR.p99 * 100.0 / refR.p99) - 100.0);
+    }
+
+    public String p99Class() {
+        if (challengerR.p99 < refR.p99) {
+            return WIN;
+        }
+        return LOOSE;
+    }
+
+    public String errorDiff() {
+        return String.format(LOCALE, "%+d", challengerR.errorCount - refR.errorCount);
+    }
+
+    public String errorClass() {
+        if (challengerR.errorCount < refR.errorCount) {
+            return WIN;
+        }
+        return LOOSE;
+    }
+    
+    public String countDiff() {
+        if (refR.count == 0) {
+            return ZERO_PERCENT;
+        }
+        return String.format(LOCALE, FORMAT_PERCENT, (challengerR.count * 100.0 / refR.count) - 100.0);
+    }
+    
+    public String countClass() {
+        return challengerR.count >= refR.count ? WIN : LOOSE;
     }
 }
